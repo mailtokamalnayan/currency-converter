0548d62f9d166fa9b66cf453e26dbaf822cd5e49
mailtokamalnayan <mailtokamalnayan@gmail.com>
2019-01-22 21:30:22 +0800

    Set up home screen

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import Home from './screens/Home';
+import EStyleSheet from "react-native-extended-stylesheet";
+
+EStyleSheet.build({
+    $primaryYellow: '#FFDC00',
+})
 
 export default () => <Home />;
