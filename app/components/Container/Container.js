143a7df9f4d1c987f6f3b3a10574c59db27a2b30
mailtokamalnayan <mailtokamalnayan@gmail.com>
2019-01-27 13:58:51 +0800

    Add gradients

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -2,9 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import { View } from "react-native";
 import styles from "./styles";
+import LinearGradient from "react-native-linear-gradient";
 
 const Container = ({ children }) => (
-  <View style={styles.container}>{children}</View>
+  <View style={styles.container}>
+    <LinearGradient
+      style={{ flex: 1 }}
+      colors={["#0F2027", "#203A43", "#2C5364"]}
+    >
+      {children}
+    </LinearGradient>
+  </View>
 );
 
 Container.propTypes = {
